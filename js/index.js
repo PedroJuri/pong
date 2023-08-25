a9d13ac16248da7184c551febe342642486bc081
PedroJuri <p3dr1n018@outlook.com>
2023-08-24 22:03:51 -0300

    feat: drawing Circle and Rect | fix: ballRadius

--- js/index.js
+++ js/index.js
@@ -7,15 +7,30 @@ canvas.height = window.innerHeight;
 const paddleWidth = 18,
 paddleHeight = 120,
 paddleSpeed = 8,
-ballRedius = 12,
+ballRadius = 12,
 initialBallSpeed = 8,
 maxBallSpeed = 40,
 netWidth = 5,
 netColor = "WHITE";
 
-// Desenhando jogadores no canvas
+// Desenhando fundos no canvas
 function drawNet(){
     for (let i =0; i <= canvas.height; i+=15){
         drawRect(canvas.width / 2 - netWidth / 2, i, netWidth, 10, netColor);
     }
+}
+
+// Desenhando jogadores no canvas
+
+function drawRect(x, y, width, height, color){
+    context.fillStyle = color;
+    context.fillRect(x, y, width, height);
+}
+
+//Desenhando a bolinha no canvas
+function drawCircle(x, y, radius, color){
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x, y, radius, 0, Math.PI * 2, false);
+    context.fill();
 }
